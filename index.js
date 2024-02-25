4e87d7e4fcac432cf50b921782d83ec65702dec1
Selene <149156667+joanapq@users.noreply.github.com>
2024-02-25 12:12:19 +0000

    added functions for engineer and intern options

--- index.js
+++ index.js
@@ -64,3 +64,63 @@ function showMenu() {
         }
     });
 }
+
+// Function to gather information about an engineer
+function gatherEngineerInfo() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the engineer's name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the engineer's employee ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the engineer's email address:"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter the engineer's GitHub username:"
+        }
+    ]).then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamMembers.push(engineer);
+        showMenu();
+    });
+}
+
+// Function to gather information about an intern
+function gatherInternInfo() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the intern's name:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the intern's employee ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the intern's email address:"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Enter the intern's school:"
+        }
+    ]).then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamMembers.push(intern);
+        showMenu();
+    });
+}
