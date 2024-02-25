1fe6ecf43ef523ba0ac03855b32c3e247fc51be4
Selene <149156667+joanapq@users.noreply.github.com>
2024-02-25 17:18:43 +0000

    updated comments in javascript files

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
 const teamMembers = [];
 
 // Function to gather information about the team manager
